def4119021967ceecb7122c139f65e2033cd9815
rexzheng <rexzheng@tencent.com>
2019-02-08 15:21:52 +0800

    fix eslint errors

--- test/proxy/basic.js
+++ test/proxy/basic.js
@@ -3,30 +3,32 @@ const Proxy = require('../../src/proxy');
 const assert = require('assert');
 
 describe('proxy.basic', () => {
-    it('Request and response must be properly proxied', async () => {
-        trans(new Proxy())
-            .post('/page?id=123')
-            .send('Topic 123...')
-            .set('X-Extended-Key', 'xyz')
-            .reply({
-                statusCode: 500,
-                statusMessage: 'Internal Database Error',
-                headers: { 'X-Res-Data': '1234abcd' },
-                body: 'update topic failed'
-            })
-            .ok(res => res.status === 500)
-            .end((err, clientReceived, serverReceived) => {
-                assert.ifError(err);
-                
-                assert.equal(serverReceived.method, 'POST');
-                assert.equal(serverReceived.path, '/page?id=123');
-                assert.equal(serverReceived.headers['X-Extended-Key'], 'xyz');
-                assert.equal(serverReceived.text, 'Topic 123...');
+  it('Request and response must be properly proxied', async () => {
+    trans(new Proxy())
+      .post('/page?id=123')
+      .send('Topic 123...')
+      .set('X-Extended-Key', 'xyz')
+      .reply({
+        statusCode: 500,
+        statusMessage: 'Internal Database Error',
+        headers: {
+          'X-Res-Data': '1234abcd'
+        },
+        body: 'update topic failed'
+      })
+      .ok(res => res.status === 500)
+      .end((err, clientReceived, serverReceived) => {
+        assert.ifError(err);
 
-                assert.equal(clientReceived.statusCode, 500);
-                assert.equal(clientReceived.statusMessage, 'Internal Database Error');
-                assert.equal(clientReceived.headers['X-Res-Data'], '1234abcd');
-                assert.equal(clientReceived.text, 'update topic failed')
-            });
-    });
+        assert.strictEqual(serverReceived.method, 'POST');
+        assert.strictEqual(serverReceived.path, '/page?id=123');
+        assert.strictEqual(serverReceived.headers['X-Extended-Key'], 'xyz');
+        assert.strictEqual(serverReceived.text, 'Topic 123...');
+
+        assert.strictEqual(clientReceived.statusCode, 500);
+        assert.strictEqual(clientReceived.statusMessage, 'Internal Database Error');
+        assert.strictEqual(clientReceived.headers['X-Res-Data'], '1234abcd');
+        assert.strictEqual(clientReceived.text, 'update topic failed');
+      });
+  });
 });
