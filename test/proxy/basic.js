a5721747585c4d21a4a7257e1d0e3ba5574575c5
rexzheng <rexzheng@tencent.com>
2019-02-09 09:38:31 +0800

    fix bug, must use await

--- test/proxy/basic.js
+++ test/proxy/basic.js
@@ -3,8 +3,9 @@ const Proxy = require('../../src/proxy');
 const assert = require('assert');
 
 describe('proxy.basic', () => {
-  it('Request and response must be properly proxied', async () => {
-    trans(new Proxy())
+  it('request and response must be properly proxied', async () => {
+    let { clientReceived, serverReceived } =
+    await trans(new Proxy())
       .post('/page?id=123')
       .send('Topic 123...')
       .set('X-Extended-Key', 'xyz')
@@ -16,19 +17,16 @@ describe('proxy.basic', () => {
         },
         body: 'update topic failed'
       })
-      .ok(res => res.status === 500)
-      .end((err, clientReceived, serverReceived) => {
-        assert.ifError(err);
+      .ok(res => res.status === 500);
 
-        assert.strictEqual(serverReceived.method, 'POST');
-        assert.strictEqual(serverReceived.path, '/page?id=123');
-        assert.strictEqual(serverReceived.headers['X-Extended-Key'], 'xyz');
-        assert.strictEqual(serverReceived.text, 'Topic 123...');
+    assert.strictEqual(serverReceived.method, 'POST');
+    assert.strictEqual(serverReceived.path, '/page?id=123');
+    assert.strictEqual(serverReceived.headers['X-Extended-Key'], 'xyz');
+    assert.strictEqual(serverReceived.text, 'Topic 123...');
 
-        assert.strictEqual(clientReceived.statusCode, 500);
-        assert.strictEqual(clientReceived.statusMessage, 'Internal Database Error');
-        assert.strictEqual(clientReceived.headers['X-Res-Data'], '1234abcd');
-        assert.strictEqual(clientReceived.text, 'update topic failed');
-      });
+    assert.strictEqual(clientReceived.statusCode, 500);
+    assert.strictEqual(clientReceived.statusMessage, 'Internal Database Error');
+    assert.strictEqual(clientReceived.headers['X-Res-Data'], '1234abcd');
+    assert.strictEqual(clientReceived.text, 'update topic failed');
   });
 });
